c486b92803ef9456cd885c104cccd39d33a79a4b
f4kkey <161198439+f4kkey@users.noreply.github.com>
2026-05-07 13:45:56 +0700

    move environment to compose

--- java/bank/src/main/java/com/khanh/util/DBconnnection.java
+++ java/bank/src/main/java/com/khanh/util/DBconnnection.java
@@ -7,9 +7,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DBconnnection {
 
     public static Connection getConnection() throws Exception {
-        String url = System.getenv("MYSQL_URL");
-        String user = System.getenv("MYSQL_USER");
-        String password = System.getenv("MYSQL_PASSWORD");
+        String url = System.getenv("MARIADB_URL");
+        String user = System.getenv("MARIADB_USER");
+        String password = System.getenv("MARIADB_PASSWORD");
         if (url == null || user == null || password == null) {
             throw new IllegalStateException(
                     "DB credentials missing. Check .env file");
